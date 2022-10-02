9f519f7a3916e91e414e54b51adf55b4388ba310
Prasetya Ikra Priyadi <prasetya.ikrapriyadi@gmail.com>
2022-10-02 22:49:36 +0700

    [Configure] Connecting to Database PostgreSQL

---
+++ config/database.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type (
+	dsn struct {
+		Host string
+		User string
+		Password string
+		Dbname string
+		Port string
+		Timezone string
+	}
+)
+
+var (
+	DB *gorm.DB
+	err error
+)
+
+func Database() {
+	dsn := dsn{
+		Host: os.Getenv("DB_HOST"),
+		User: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Dbname: os.Getenv("DB_NAME"),
+		Port: os.Getenv("DB_PORT"),
+		Timezone: os.Getenv("DB_TIMEZONE"),
+	}
+	db_url := "host="+dsn.Host+" user="+dsn.User+" password="+dsn.Password+" dbname="+dsn.Dbname+" port="+dsn.Port+" TimeZone="+dsn.Timezone
+	DB, err = gorm.Open(postgres.Open(db_url), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database Connected")
+}
+
+// func Migrate() {
+// 	DB.AutoMigrate(&entity.User{})
+// }
--- server.go
+++ server.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"ems-aquadev/config"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	//Initialize Database
+	config.Database()
+
+	//echo instances
 	e := echo.New()
 
 	e.GET("/", HomeAPI)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(os.Getenv("SERVER_PORT")))
 }
 
 func HomeAPI(c echo.Context) error{
